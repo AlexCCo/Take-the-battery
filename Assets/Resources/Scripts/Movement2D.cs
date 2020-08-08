2032522f90f6b62c1ae8d9064d72ba9bf0be561d
alejandro cancelo <canceloalejandro@gmail.com>
2020-08-08 22:42:11 +0200

    Character controller created

--- Assets/Resources/Scripts/Movement2D.cs
+++ Assets/Resources/Scripts/Movement2D.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour {
 
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 1f;
     [SerializeField] private float jumpAltitud = 10f;
+
+    [SerializeField] private GameObject groundCheck;
+    // must be set to the layer of the elements your character can step on
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+
     private Animator animator;
+    private float isIdleRightDirection;
+
     private Rigidbody2D characterBody;
     // x horizontal, y jumping
     private Vector2 movement;
-    private float isIdleRightDirection;
+    private Vector2 fixedMovement;
+    private Vector2 zeroVector;
+    private float movementSmoothingTime = 0.05f;
 
     private void Start() {
         characterBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        Vector2 movement = new Vector2(0, 0);
+        movement = new Vector2(0, 0);
+        fixedMovement = new Vector2(0, 0);
+        zeroVector = Vector2.zero;
         isIdleRightDirection = 1f;
-}
+    }
 
     // Update is called once per frame
     void Update(){
@@ -28,10 +40,7 @@ public class Movement2D : MonoBehaviour {
             // Otherwise it means we are looking to the left
             isIdleRightDirection = movement.x;
         }
-
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Jump");
-
+        
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Speed", Math.Abs(movement.x) * speed);
         animator.SetFloat("isIdleRightDirection", isIdleRightDirection);
@@ -39,10 +48,20 @@ public class Movement2D : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        Debug.Log("Horizontal: " + movement.x + "  jump: " + movement.y);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Jump");
 
-        movement.x *= speed;
-        movement.y *= jumpAltitud;
-        characterBody.MovePosition(characterBody.position + movement * Time.fixedDeltaTime);
+        Collider2D[] maxCollider = Physics2D.OverlapCircleAll(groundCheck.transform.position, groundCheckRadius, groundLayer);
+
+        if (maxCollider.Length > 0 && movement.y > 0) {
+            fixedMovement.y = movement.y * jumpAltitud;
+            characterBody.AddForce(fixedMovement, ForceMode2D.Impulse);
+        }
+
+        Vector2 targetVelocity = new Vector2(movement.x * speed * 10f, characterBody.velocity.y);
+        Vector2 smoothVelocity = Vector2.SmoothDamp(characterBody.velocity, targetVelocity, ref zeroVector, movementSmoothingTime);
+
+        characterBody.velocity = smoothVelocity;
     }
+
 }
